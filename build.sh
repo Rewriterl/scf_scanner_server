53a1d5c892f38c6746bdc603161fc0b25692d570
lapponia <50308722+Rewriterl@users.noreply.github.com>
2023-12-22 12:07:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -ldflags "-w -s" -o scf_bootstrap
  zip -r scf_bootstrap.zip scf_bootstrap tools/
- echo "test echo"
- ls -al tools/
- chmod +x tools/*
  rm scf_bootstrap
