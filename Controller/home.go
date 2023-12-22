002a08515cc5f5c8bebf08ab9257b0ce91c58faa
lapponia <50308722+Rewriterl@users.noreply.github.com>
2023-12-22 12:59:35 +0800

    Update home.go

--- Controller/home.go
+++ Controller/home.go
@@ -11,8 +11,8 @@ func HomePage(c *gin.Context) {
 	c.String(http.StatusOK, "!!!!!Hello World!!!!!")
 	cmd := exec.Command("chmod", "+x", "./tools/f")
     	out,err := cmd.CombinedOutput()
-	exec.Command("chmod", "+x", "./tools/gd")
-	exec.Command("chmod", "+x", "./tools/k")
+	cmd = exec.Command("chmod", "+x", "./tools/gd")
+	cmd = exec.Command("chmod", "+x", "./tools/k")
 	cmd = exec.Command("ls","tools/","-al")
 	out,err = cmd.CombinedOutput()
 	if (err==nil){
