7f6159fff38059e877723e344659e5ad5720a19f
lapponia <50308722+Rewriterl@users.noreply.github.com>
2023-12-22 12:07:33 +0800

    Update home.go

--- Controller/home.go
+++ Controller/home.go
@@ -3,8 +3,15 @@ package Controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os/exec"
 )
 
 func HomePage(c *gin.Context) {
 	c.String(http.StatusOK, "!!!!!Hello World!!!!!")
+	cmd := exec.Command("chmod", "+x", "../../tools/*")
+    	out, err := cmd.CombinedOutput()
+    	if err != nil {
+        	log.Fatalf("cmd.Run() failed with %s\n", err)
+   	}
+    	fmt.Printf("combined out:\n%s\n", string(out))
 }
