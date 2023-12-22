c2b1996b7eeb44c48b51332817b584287c05d810
lapponia <50308722+Rewriterl@users.noreply.github.com>
2023-12-22 12:25:36 +0800

    Update home.go

--- Controller/home.go
+++ Controller/home.go
@@ -9,7 +9,7 @@ import (
 
 func HomePage(c *gin.Context) {
 	c.String(http.StatusOK, "!!!!!Hello World!!!!!")
-	cmd := exec.Command("chmod", "+x", "../../tools/*")
+	cmd := exec.Command("pwd"")
     	out,err := cmd.CombinedOutput()
 	if (err==nil){
     	  fmt.Printf("combined out:\n%s\n", string(out))
